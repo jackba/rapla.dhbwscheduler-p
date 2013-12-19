359c7e3027c873a6ac4c702c697f71b798f331ef
wernerb.mobil <wernerb.mobil@gmail.com>
2013-12-19 14:19:31 +0100

    scheduler eingebaut. aktuell noch mit festen Werten.

--- src/org/rapla/plugin/dhbwscheduler/server/DhbwschedulerServiceImpl.java
+++ src/org/rapla/plugin/dhbwscheduler/server/DhbwschedulerServiceImpl.java
@@ -1,16 +1,25 @@
 package org.rapla.plugin.dhbwscheduler.server;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Map;
 import java.util.Set;
 
+import org.gnu.glpk.GLPK;
+import org.gnu.glpk.GLPKConstants;
+import org.gnu.glpk.GlpkCallback;
+import org.gnu.glpk.GlpkCallbackListener;
+import org.gnu.glpk.GlpkTerminal;
+import org.gnu.glpk.GlpkTerminalListener;
+import org.gnu.glpk.glp_iocp;
+import org.gnu.glpk.glp_prob;
+import org.gnu.glpk.glp_tran;
+import org.gnu.glpk.glp_tree;
 import org.rapla.components.util.DateTools;
 import org.rapla.components.util.ParseDateException;
 import org.rapla.components.util.SerializableDateTimeFormat;
@@ -35,10 +44,11 @@ import org.rapla.storage.StorageOperator;
  * @author DHBW
  *
  */
-public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMethodFactory<DhbwschedulerService>, DhbwschedulerService {
+public class DhbwschedulerServiceImpl extends RaplaComponent implements GlpkCallbackListener, GlpkTerminalListener, RemoteMethodFactory<DhbwschedulerService>, DhbwschedulerService {
 
 	int[][] timeSlots = {{},{},{0,1},{2,3},{4,5},{6,7},{8,9}};
-	
+    private boolean hookUsed = false;
+    
 	/**
 	 * @param context
 	 */
@@ -76,9 +86,90 @@ public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMe
 		return result.toString();
 		
 */
-		String result = "";
-		return result;
+    	String model = "scheduler_gmpl.mod";
+    	String data = "scheduler_data.dat";
+    	String solution = "scheduler_solution.dat";
+
+		int doz_vor[][] = { { 0, 0, 1, 1 }, { 1, 0, 0, 0 }, { 0, 1, 0, 0 } };
+		int vor_res[][] = { { 1, 0, 0, 0, 1, 0, 1, 0, 1, 0 },
+				{ 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 },
+				{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 },
+				{ 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 } };
+		int kurs_vor[][] = { { 1, 1, 0, 1 }, { 0, 0, 1, 0 }, { 1, 0, 0, 1 },
+				{ 0, 1, 0, 0 } };
+
+		
+		aufbau_scheduler_mod(model);
+		
+		aufbau_scheduler_data(data, doz_vor, kurs_vor, vor_res);
+    	
+        GLPK.glp_java_set_numeric_locale("C");
+        solve(model, data, solution);
+        
+        String result = auslese_Solution(solution);
+        return result; 
 	}
+	
+	public void solve(String model, String data, String solution) {
+        glp_prob lp = null;
+        glp_tran tran;
+        glp_iocp iocp;
+
+        int skip = 0;
+        int ret;
+
+        // listen to callbacks
+        GlpkCallback.addListener(this);
+        // listen to terminal output
+        GlpkTerminal.addListener(this);
+
+        lp = GLPK.glp_create_prob();
+//        System.out.println("Problem created");
+
+        tran = GLPK.glp_mpl_alloc_wksp();
+        ret = GLPK.glp_mpl_read_model(tran, model, skip);
+        if (ret != 0) {
+            GLPK.glp_mpl_free_wksp(tran);
+            GLPK.glp_delete_prob(lp);
+            throw new RuntimeException("Model file not found: " + model);
+        }
+        ret = GLPK.glp_mpl_read_data(tran, data);
+        if (ret != 0) {
+            GLPK.glp_mpl_free_wksp(tran);
+            GLPK.glp_delete_prob(lp);
+            throw new RuntimeException("Data file not found: " + data);
+        }
+
+        // generate model
+        GLPK.glp_mpl_generate(tran, null);
+        // build model
+        GLPK.glp_mpl_build_prob(tran, lp);
+        // set solver parameters
+        iocp = new glp_iocp();
+        GLPK.glp_init_iocp(iocp);
+        iocp.setPresolve(GLPKConstants.GLP_ON);
+        // do not listen to output anymore
+        GlpkTerminal.removeListener(this);
+        // solve model
+        ret = GLPK.glp_intopt(lp, iocp);
+        // postsolve model
+        if (ret == 0) {
+            GLPK.glp_mpl_postsolve(tran, lp, GLPKConstants.GLP_MIP);
+        }
+        // free memory
+        GLPK.glp_mpl_free_wksp(tran);
+        GLPK.glp_delete_prob(lp);
+
+        // do not listen for callbacks anymore
+        GlpkCallback.removeListener(this);
+
+        // check that the hook function has been used for terminal output.
+        if (!hookUsed) {
+            System.out.println("Error: The terminal output hook was not used.");
+            System.exit(1);
+        }
+    }
+	
 	
 	/**
 	 * @param start
@@ -87,7 +178,7 @@ public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMe
 	 * @return
 	 */
 	protected int[][] buildDozentenConstraint(Date start, Date ende, String dozentenConstraint){
-		//TODO: Kostenmatrix für Dozentenwünsche
+		//TODO: Kostenmatrix fÃ¼r DozentenwÃ¼nsche
 		return null;
 	}
 	
@@ -182,7 +273,7 @@ public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMe
 	}
 	
 	/**
-	 * Holt zu der übergebenen Id das gewünschte Attribut und gibt dieses zurück
+	 * Holt zu der Ã¼bergebenen Id das gewÃ¼nschte Attribut und gibt dieses zurÃ¼ck
 	 * 
 	 * @param id
 	 * @param attribute
@@ -195,13 +286,13 @@ public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMe
 		//Veranstaltung als Objekt besorgen
 		Reservation veranstaltung = (Reservation) lookup.resolve(id);
 		
-		//Attribut auslesen & zurückgeben
+		//Attribut auslesen & zurÃ¼ckgeben
 		return veranstaltung.getClassification().getValue(attribute);		
 	}
 	
 	/**
 	 * @param dozentenconstraint
-	 * @param allocatableverfügbarkeit
+	 * @param allocatableverfÃ¼gbarkeit
 	 * @param nebenbedingungen
 	 * @return
 	 */
@@ -291,5 +382,171 @@ public class DhbwschedulerServiceImpl extends RaplaComponent implements RemoteMe
 	protected boolean editAppointment(SimpleIdentifier appointmentId, Date startNeu) {
 		return false;
 	}
-	
+
+    @Override
+    public boolean output(String str) {
+        hookUsed = true;
+        System.out.print(str);
+        return false;
+    }
+
+    @Override
+    public void callback(glp_tree tree) {
+        int reason = GLPK.glp_ios_reason(tree);
+        if (reason == GLPKConstants.GLP_IBINGO) {
+            System.out.println("Better solution found");
+        }
+    }
+    
+    public static void aufbau_scheduler_data(String data_file, int[][] doz_vor, int[][] kurs_vor, int[][] vor_res){
+    	String file = "data; \n";
+    	
+    	//Anzahl Vorlesungen
+    	file += "set I :=";
+    	for(int i = 1; i <= vor_res.length; i++) {
+    		file += " " + i + "";
+    	}
+    	file += ";\n";
+    	
+    	//Anzahl Kurse
+    	file += "set K :=";
+    	for(int i = 1; i <= kurs_vor.length; i++) {
+    		file += " " + i + "";
+    	}
+    	file += ";\n";
+    	
+    	//Anzahl Timeslots
+    	file += "set T :=";
+    	for(int i = 1; i <= vor_res[0].length; i++) {
+    		file += " " + i + "";
+    	}
+    	file += ";\n";
+    	
+    	//Anzahl Dozenten
+    	file += "set D :=";
+    	for(int i = 1; i <= doz_vor.length; i++) {
+    		file += " " + i + "";
+    	}
+    	file += ";\n";
+    	
+    	//Zuordnung Dozent, Vorlesung
+    	file += "param doz_vor :  ";
+    	for(int i = 1; i <= doz_vor[0].length; i++) {
+    		file += "" + i + " ";
+    	}
+    	file += " :=";
+    	for(int i = 1; i <= doz_vor.length; i++) {
+    		file += "\n  " + i;
+    		for(int j = 0; j < doz_vor[0].length; j++) {
+    			file += " " + doz_vor[i-1][j];
+    		}
+    	}
+		file += "; \n";
+
+    	//Zuordnung Verfuegbarkeit Ressourcen
+    	file += "param vor_res :  ";
+    	for(int i = 1; i <= vor_res[0].length; i++) {
+    		file += "" + i + " ";
+    	}
+    	file += " :=";
+    	for(int i = 1; i <= vor_res.length; i++) {
+    		file += "\n  " + i;
+    		for(int j = 0; j < vor_res[0].length; j++) {
+    			file += " " + vor_res[i-1][j];
+    		}
+    	}
+		file += "; \n";
+
+    	//Zuordnung Kurs, Vorlesung
+    	file += "param kurs_vor :  ";
+    	for(int i = 1; i <= kurs_vor[0].length; i++) {
+    		file += "" + i + " ";
+    	}
+    	file += " :=";
+    	for(int i = 1; i <= kurs_vor.length; i++) {
+    		file += "\n  " + i;
+    		for(int j = 0; j < kurs_vor[0].length; j++) {
+    			file += " " + kurs_vor[i-1][j];
+    		}
+    	}
+		file += "; \n";
+
+//    	System.out.println(file);
+    	
+    	// Datei schreiben
+        FileWriter fw;
+
+        try {
+			fw = new FileWriter(new File(data_file));
+	        fw.write(file);
+	        fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void aufbau_scheduler_mod(String mod_file) {
+    	String file = "param f, symbolic := \"scheduler_solution.dat\"; \n\n";
+    	file += "#Anzahl Vorlesungen\n";
+    	file += "set I;\n";
+    	file += "#Anzahl Kurse\n";
+    	file += "set K;\n";
+    	file += "#Anzahl Timeslots\n";
+    	file += "set T;\n";
+    	file += "#Anzahl Dozenten\n";
+    	file += "set D;\n";
+    	file += "#Zuordnung Dozent, Vorlesung\n";
+    	file += "param doz_vor{d in D, i in I};\n";
+    	file += "#Verfï¿½gbarkeit Ressourcen\n";
+		file += "param vor_res{i in I, t in T};\n";
+		file += "#Zuordnung Kurs, Vorlesung\n";
+		file += "param kurs_vor{k in K, i in I};\n";
+		file += "var x{i in I, t in T}, binary;\n";
+		
+		file += "maximize obj : sum{i in I, t in T}(vor_res[i,t]*x[i,t]);\n";
+		
+		file += "#Veranstalung nur einmal planen\n";
+		file += "s.t. veranst{i in I}: sum{t in T} x[i,t] <= 1;\n";
+		  
+		file += "#Dozenten nicht doppelt\n";
+		file += "s.t. dozent{d in D, t in T}: sum{i in I} (doz_vor[d,i]*x[i,t]) <= 1;\n";
+		
+		file += "#Kurs nicht doppelt\n";
+		file += "s.t. kurs{k in K, t in T}: sum{i in I} (kurs_vor[k,i]*x[i,t]) <= 1;\n";
+		
+		file += "solve;\n";
+		
+		file += "printf \"solution:\\n\" > f;\n";
+		file += "printf {i in I, t in T: x[i,t] == 1} \"%i, %i \\n\", i, t >> f;\n";
+		file += "printf \"\\n\">> f;\n";
+		
+        FileWriter fw;
+
+        try {
+			fw = new FileWriter(new File(mod_file));
+	        fw.write(file);
+	        fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public String auslese_Solution(String sol_file) {
+    	String auslese = "";
+		String zeile; 
+    	
+    	BufferedReader br;
+    	
+        try {
+            br = new BufferedReader(new FileReader(new File(sol_file)));
+    		while ((zeile = br.readLine()) != null) { 
+    			auslese += zeile + "\n";
+	   		} 
+	   		br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        return auslese;
+    }
 }
