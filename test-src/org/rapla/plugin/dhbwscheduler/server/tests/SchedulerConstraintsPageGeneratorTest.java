d9a62a468bf7e7e71dc5bd043b411fa9ababe1aa
FlickingerMarco90 <{FlickingerMarco90}@gmail.com>
2014-05-09 12:18:20 +0200

    Test erweiterung

--- test-src/org/rapla/plugin/dhbwscheduler/server/tests/SchedulerConstraintsPageGeneratorTest.java
+++ test-src/org/rapla/plugin/dhbwscheduler/server/tests/SchedulerConstraintsPageGeneratorTest.java
@@ -3,6 +3,8 @@ package org.rapla.plugin.dhbwscheduler.server.tests;
 import static org.mockito.Mockito.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.ServletContext;
@@ -13,10 +15,17 @@ import javax.servlet.http.HttpServletResponse;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.omg.CORBA.Request;
 import org.rapla.RaplaTestCase;
+import org.rapla.components.xmlbundle.impl.I18nBundleImpl;
+import org.rapla.entities.domain.Allocatable;
+import org.rapla.entities.domain.Appointment;
+import org.rapla.entities.domain.Reservation;
 import org.rapla.facade.ClientFacade;
 import org.rapla.framework.Configuration;
 import org.rapla.framework.DefaultConfiguration;
+import org.rapla.framework.RaplaException;
+import org.rapla.plugin.dhbwscheduler.DhbwschedulerPlugin;
 import org.rapla.plugin.dhbwscheduler.server.ConstraintService;
 import org.rapla.plugin.dhbwscheduler.server.SchedulerConstraintsPageGenerator;
 
@@ -34,6 +43,7 @@ public class SchedulerConstraintsPageGeneratorTest  extends RaplaTestCase {
     protected void setUp() throws Exception {
         super.setUp();
         facade = getContext().lookup(ClientFacade.class );
+        raplaContainer.addContainerProvidedComponent( DhbwschedulerPlugin.RESOURCE_FILE, I18nBundleImpl.class,I18nBundleImpl.createConfig( DhbwschedulerPlugin.RESOURCE_FILE.getId() ) );
         facade.login("homer","duffs".toCharArray());
         locale = Locale.getDefault();
     }
@@ -71,13 +81,48 @@ public class SchedulerConstraintsPageGeneratorTest  extends RaplaTestCase {
     	assertEquals(ergebnis,pg.formatDateForDatepicker(test));
     }
     
-    public void testegeneratePage(){
+    public void testegeneratePage() throws Exception{
     	DefaultConfiguration config = new DefaultConfiguration("locale"); 
     	SchedulerConstraintsPageGenerator pg = new SchedulerConstraintsPageGenerator(getContext(),config);
     	try {	
     		HttpServletRequest request = mock(HttpServletRequest.class);
     		HttpServletResponse response = mock(HttpServletResponse.class);
     		ServletContext context = mock(ServletContext.class);
+    		
+    		Reservation r = facade.newReservation();
+    		String id = r.getId();
+    		
+    		Date startDate = getRaplaLocale().toRaplaDate(2014, 5, 11);
+	        Date endDate = getRaplaLocale().toRaplaDate(2014, 5, 12);
+	        Appointment appointment = facade.newAppointment(startDate, endDate);
+	        r.addAppointment(appointment);
+
+	        r.getClassification().setValue("title", "test");
+
+			Allocatable pers1 = facade.newPerson();
+			Allocatable pers2 = facade.newPerson();
+			
+			
+			pers1.getClassification().setValue("surname", "Wurst");
+			pers1.getClassification().setValue("firstname", "Hans");
+			pers1.getClassification().setValue("email", "test@test.avdfdfefdgt");
+			
+			pers2.getClassification().setValue("surname", "Pan");
+			pers2.getClassification().setValue("firstname", "Peter");
+			
+			facade.store(pers1);
+			facade.store(pers2);
+			r.addAllocatable(pers1);
+			r.addAllocatable(pers2);
+    		
+    		facade.store(r);
+    		
+    		when(request.getParameter("id")).thenReturn(r.getId());
+    		when(request.getParameter("dozent")).thenReturn(pers1.getId());
+    		PrintWriter writer = new PrintWriter("servlettest.txt");
+            when(response.getWriter()).thenReturn(writer);
+    		
+    		
 			pg.generatePage(context, request, response);
 		} catch (IOException | ServletException e) {
 			e.printStackTrace();
